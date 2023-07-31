c02a6b4cccac9b958a4b24471e4dfc12bd31bda5
AnosikeChidinma <anosikechidinma@gmail.com>
2023-07-31 22:15:30 +0100

    vendors and popular vendors

--- src/routes/vendorRoutes.ts
+++ src/routes/vendorRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import {verifyVendor, registerVendor} from "../controllers/vendorControllers"
+import {verifyVendor, registerVendor, getPopularVendors, getAllVendors} from "../controllers/vendorControllers"
 import { authForVerifiedVendor, auth } from '../middleware/authorizations';
 import { upload } from '../middleware/upload';
 
@@ -8,6 +8,10 @@ const router = express.Router();
 
 router.post('/verifyvendor', verifyVendor)
 router.post('/registervendor', authForVerifiedVendor, upload.single('cover_image'), registerVendor)
+router.get('/getPopularVendors', getPopularVendors)
+router.get('/getVendors', getAllVendors)
+
+
 
 
 export default router;
