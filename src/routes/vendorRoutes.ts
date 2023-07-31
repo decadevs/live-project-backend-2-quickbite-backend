88d6ec04cad26d31131bb35b185b362517182f6b
Decagon <decagon@macs-MacBook-Air.local>
2023-07-31 22:41:33 +0100

    vendor creates food

--- src/routes/vendorRoutes.ts
+++ src/routes/vendorRoutes.ts
@@ -1,13 +1,15 @@
-import express from 'express'
-import {verifyVendor, registerVendor} from "../controllers/vendorControllers"
-import { authForVerifiedVendor, auth } from '../middleware/authorizations';
-import { upload } from '../middleware/upload';
-
+import express from "express";
+import { verifyVendor, registerVendor } from "../controllers/vendorControllers";
+import { authForVerifiedVendor, auth } from "../middleware/authorizations";
+import { upload } from "../middleware/upload";
+import {vendorcreatesFood, vendorgetsAllFood, vendorGetsSingleFood, } from "../controllers/vendorControllers";
 
 const router = express.Router();
 
-router.post('/verifyvendor', verifyVendor)
-router.post('/registervendor', authForVerifiedVendor, upload.single('cover_image'), registerVendor)
-
+router.post("/verifyvendor", verifyVendor);
+router.post("/registervendor", authForVerifiedVendor, upload.single("cover_image"), registerVendor);
+router.post("/createfood", auth, upload.single("food_image"), vendorcreatesFood);
+router.get("/getallfood", auth, vendorgetsAllFood);
+router.get("/getsinglefood", auth, vendorGetsSingleFood);
 
 export default router;
