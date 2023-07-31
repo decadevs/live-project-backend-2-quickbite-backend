6c81d310d010874af9cf0aa3447ff2a3e9de5ad4
Jidekas <132021739+Jidekas@users.noreply.github.com>
2023-07-31 22:10:30 +0100

    register_user

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,8 +1,14 @@
-import { Router } from "express";
+import { registerUser, LogIn, verifyOtp, reSendOtp} from '../controllers/userControllers';
+import {Router} from 'express';
 import {auth} from '../middleware/authorizations'
-
-
 
 const router = Router();
 
-export default router
+router.post('/register', registerUser);
+router.post('/login', LogIn);
+router.post('/verify', auth, verifyOtp);
+router.get('/resend', auth, reSendOtp);
+
+
+
+export default router;
